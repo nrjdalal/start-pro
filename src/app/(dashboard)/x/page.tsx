eb890cbfe90d73e5c2d4694acd5df618092dd02a
Neeraj Dalal <admin@nrjdalal.com>
2025-12-08 01:47:31 +0530

    chore: save state

--- src/app/(dashboard)/x/page.tsx
+++ src/app/(dashboard)/x/page.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute("/(dashboard)/x")({
+export const Route = createFileRoute("/_layout/x/")({
   component: Page,
 })
 
