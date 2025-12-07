63b0460a9e6cef67d0c70df1c748e7a4c97d2103
Neeraj Dalal <admin@nrjdalal.com>
2025-12-07 23:10:31 +0530

    chore: update provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,16 +1,23 @@
-"use client"
-
-import { useState } from "react"
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { Toaster } from "sonner"
 
 import DevTools from "@/components/devtools"
 
-export function OuterProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+export function getContext() {
+  const queryClient = new QueryClient()
+  return {
+    queryClient,
+  }
+}
 
+export function OuterProvider({
+  children,
+  queryClient,
+}: {
+  children: React.ReactNode
+  queryClient: QueryClient
+}) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
